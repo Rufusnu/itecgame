11313d392c1539857a353fb17865edd3c443b306
Xabifk <adelintataru87@gmail.com>
2018-11-24 17:13:33 +0200

    Add attack animation

--- Scripts/AttackArea.gd
+++ Scripts/AttackArea.gd
@@ -4,9 +4,13 @@ export (int) var E_DAMAGE
 
 var g_knock_velo = Vector2(100, -100)
 
-func setup(facing_right):
+func setup(dist, facing_right):
+	
 	if !facing_right:
 		g_knock_velo.x = -g_knock_velo.x
+		dist = -dist
+	position.x += dist
+	
 
 func _on_AttackArea_body_entered(body):
 	if body.name != "Player":
@@ -14,3 +18,12 @@ func _on_AttackArea_body_entered(body):
 			body.take_damage(E_DAMAGE)
 		if body.has_method("knock"):
 			body.knock(g_knock_velo)
+
+
+func _on_Timer_timeout():
+	queue_free()
+
+
+func _on_StartTimer_timeout():
+	$CollisionShape2D.disabled = false
+	$Timer.start()
