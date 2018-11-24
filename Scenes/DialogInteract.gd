523c6e3b0a8722b8d2931d73703daa6820cadd5c
Xabifk <adelintataru87@gmail.com>
2018-11-24 19:58:43 +0200

    Add visible component to Dialog interact

--- Scenes/DialogInteract.gd
+++ Scenes/DialogInteract.gd
@@ -4,18 +4,22 @@ export (String) var E_TEXT
 
 func _ready():
 	$Label.text = E_TEXT
-	visible = false
+	$ColorRect.visible = false
+	$Label.visible = false
+	$Sprite.visible = false
 
 func interact():
-	if visible and ($Label.visible_characters >= E_TEXT.length() or $Label.visible_characters == -1):
+	if $Label.visible and ($Label.visible_characters >= E_TEXT.length() or $Label.visible_characters == -1):
 		$Label.visible_characters = 0
-		visible = false
+		$ColorRect.visible = false
+		$Label.visible = false
 		$Timer.stop()
-	elif visible:
+	elif $Label.visible:
 		$Label.visible_characters = -1
 		$Timer.stop()
 	else:
-		visible = true
+		$ColorRect.visible = true
+		$Label.visible = true
 		$Timer.start()
 
 func _on_Timer_timeout():
@@ -23,7 +27,14 @@ func _on_Timer_timeout():
 	
 
 func _on_DialogInteract_area_exited(area):
-	print(area.name)
 	if area.get_parent().name == "Player":
-		visible = false
+		$ColorRect.visible = false
+		$Label.visible = false
 		$Label.visible_characters = 0
+		$Timer.stop()
+		$Sprite.visible = false
+
+
+func _on_DialogInteract_area_entered(area):
+	if area.get_parent().name == "Player":
+		$Sprite.visible = true
